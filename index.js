65f436afaf0c07c686ab57f556238223552b43c4
gatosovietico <164811514+gatosovietico@users.noreply.github.com>
2024-12-08 23:21:21 -0600

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const yesBtn = document.querySelector('#yesBtn');
 const noBtn = document.querySelector('#noBtn');
-const message = document.querySelector('.contenedor p'); // Selecciona el mensaje
+const message = document.querySelector('.contenedor p'); 
 
 const phrases = [
     "¿Estás segura?",
@@ -11,23 +11,19 @@ const phrases = [
     "¿De verdad no?"
 ];
 
-// Cuando haces clic en el botón "Sí"
 yesBtn.addEventListener('click', function () {
     alert('Te amooooooo❤');
 });
 
-// Cuando pasas el mouse sobre el botón "No"
+
 noBtn.addEventListener('mouseover', function () {
-    // Generar posiciones aleatorias entre 0 y 90
     const randomX = Math.random() * 90;
     const randomY = Math.random() * 90;
 
-    // Mover el botón
     noBtn.style.top = randomY + '%';
     noBtn.style.left = randomX + '%';
     noBtn.style.transform = `translate(-${randomX}%, -${randomY}%)`;
 
-    // Cambiar el mensaje
     const randomIndex = Math.floor(Math.random() * phrases.length);
     message.textContent = phrases[randomIndex];
 });
